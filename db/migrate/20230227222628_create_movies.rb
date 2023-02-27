364975b2ad9a4928febdbc43e0115b9a46a8dcfb
Tainan Bogo <tainanbogo@gmail.com>
2023-02-27 20:28:02 -0300

    done

--- db/migrate/20230227222628_create_movies.rb
+++ db/migrate/20230227222628_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration[7.0]
   def change
     create_table :movies do |t|
       t.string :title
-      t.string :onverview
+      t.text :overview
       t.string :poster_url
       t.float :rating
 
